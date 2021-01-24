befbf4e1d675967194d3959dbce7500ec78ff0db
Sztivt <microphonefreak@gmail.com>
2021-01-24 05:36:44 +0100

    Stage two

--- Numeral System Converter/task/src/converter/Main.kt
+++ Numeral System Converter/task/src/converter/Main.kt
@@ -5,33 +5,54 @@ import kotlin.math.pow
 
 val scanner = Scanner(System.`in`)
 fun main() {
-    // write your code here
-    print(baseEight(scanner.nextInt()))
+    print(convertDec(scanner.nextInt(), scanner.nextInt()))
 }
 
-fun binary(num: Int): String {
-    var remainder = num
-    var binary = "0b"
-    var pow = 0
-    do {
-        pow++
-    } while (remainder > 2.0.pow(pow))
-    for (i in 0..pow) {
-        if (remainder == 1) {
-            binary += 1
-            break
-        }
-        val divider = 2.0.pow(pow - i).toInt()
-        if (remainder >= divider) {
-            binary += "1"
-            remainder -= divider
-        } else {
-            binary += 0
-        }
+fun convertDec(num: Int, base: Int): String {
+    var retString = when (base) {
+        2 -> "0b"
+        8 -> "0"
+        16 -> "0x"
+        else -> ""
     }
-    return binary
+    var remainder = num
+    var power = 0
+    do {
+        power++
+    } while (remainder.toDouble() > base.toDouble().pow(power))
+    if (remainder == base.toDouble().pow(power).toInt()) {
+        retString += "1"
+        for (p in 0 until power) {
+            retString += "0"
+        }
+        return retString
+    }
+    for (p in power - 1 downTo 0) {
+        val powered = base.toDouble().pow(p).toInt()
+        val times = remainder / powered
+        remainder -= powered * times
+        retString += numToSign(times)
+    }
+    return retString
 }
 
-fun baseEight(num: Int): Int {
-    return num % 8
+fun numToSign(num: Int): String {
+    return when (num) {
+        1 -> "1"
+        2 -> "2"
+        3 -> "3"
+        4 -> "4"
+        5 -> "5"
+        6 -> "6"
+        7 -> "7"
+        8 -> "8"
+        9 -> "9"
+        10 -> "a"
+        11 -> "b"
+        12 -> "c"
+        13 -> "d"
+        14 -> "e"
+        15 -> "f"
+        else -> "0"
+    }
 }
